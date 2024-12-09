5c1ad2093246032a599e8010ab36915ae342aafd
김지훈 <kigpand@gmail.com>
2024-12-09 22:30:33 +0900

    feat: view용 컴포넌트 추가(추후 삭제 필요)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/reset.css";
-import { Carousel } from ".";
+import View from "./View";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Carousel />
+    <View />
   </StrictMode>
 );
