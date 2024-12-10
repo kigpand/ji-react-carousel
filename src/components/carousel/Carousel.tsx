769eb5cbe632f2d1db7452a5911236e3d2aedd0d
김지훈 <kigpand@gmail.com>
2024-12-10 23:22:07 +0900

    comment: Carousel 동작관련 주석 추가

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -7,6 +7,11 @@ import React, {
 } from "react";
 import CarouselSlider from "./CarouselSlider";
 
+/**
+ * width: Carousel Item의 width
+ * viewCount: Carousel에서 한번에 보여줄 item 갯수
+ * infinite: 무한 슬라이드 적용 여부. defalut = false
+ */
 type Props = {
   width: number;
   viewCount: number;
@@ -20,8 +25,15 @@ export function Carousel({
   infinite = false,
 }: Props) {
   const [moveCount, setMoveCount] = useState<number>(infinite ? viewCount : 0);
+  // transition용 state
   const [transition, setTransition] = useState<string>("500ms");
 
+  /**
+   * slider에 적용할 Child 요소
+   * viewCount만큼 양 옆으로 임시 아이템 추가.
+   * 임시 아이템를 만든 이유는 양 사이드 끝에 도달했을 경우 다음 위치로 이동할려면 transtion이 해당 위치로 이동하기 때문에 UX적인 이슈가 있음.
+   * 그래서 임시 아이템를 만들어 임시아이템 위치에 도달하면 transtion을 없애고 실제 아이템 위치로 이동하도록하기 위해 임시 아이템 추가.
+   *  */
   const sliderChildren = useMemo<ReactElement[]>(() => {
     const childArr = React.Children.toArray(children);
     if (infinite) {
@@ -44,6 +56,9 @@ export function Carousel({
   const handleLeftButton = () => {
     if (infinite) {
       setMoveCount(moveCount - 1);
+
+      // 현재 아이템 위치가 viewCount와 같다는건 임시 아이템 위치에 도달했다는 것.
+      // 임시 아이템에서 원래 보여줘야할 실제 아이템로 이동하기 위한 구문.
       if (moveCount === viewCount) {
         const childLength = React.Children.count(children);
         handleMoveToSlide(childLength + viewCount - 1);
@@ -70,6 +85,11 @@ export function Carousel({
     setTransition("500ms");
   };
 
+  /**
+   * 임시 아이템에 도달했을 경우 실행할 함수.
+   * transtion을 없애서 임시 아이템에서 실제 아이템으로 이동할때 UX의 문제를 없앰.
+   * TODO: setTimeout seconds 상수화 필요.
+   *  */
   const handleMoveToSlide = (num: number) => {
     setTimeout(() => {
       setTransition("");
