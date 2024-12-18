70481d96e2759368fc95c844b5e1d3b5ac7831e6
김지훈 <kigpand@gmail.com>
2024-12-18 21:12:29 +0900

    feat: 배포 세팅 완료(배포 확인)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "src/index.ts", // 라이브러리 진입점
+      name: "JiReactCarousel", // 글로벌 네임스페이스 이름
+      fileName: (format) => `ji-react-carousel.${format}.js`, // 파일 이름
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"], // React는 외부 의존성 처리
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
