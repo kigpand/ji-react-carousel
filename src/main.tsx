b72a0a4a61b07153f30daa10237bfcdec6f40121
김지훈 <kigpand@gmail.com>
2024-12-21 12:59:30 +0900

    test: github action publishing 테스트

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/reset.css";
-import { Carousel } from ".";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Carousel />
   </StrictMode>
 );
