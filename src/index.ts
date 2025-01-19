ef4fde9465c5dd5ed6335e46d08bc1881f0e37bc
김지훈 <kigpand@gmail.com>
2025-01-19 22:38:07 +0900

    -f bannerCarousel export 코드 추가

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { Carousel } from "./components/carousel/Carousel";
 export { PagingCarousel } from "./components/carousel/PagingCarousel";
 
 export { FocusCarousel } from "./components/carousel/FocusCarousel";
+
+export { BannerCarousel } from "./components/carousel/BannerCarousel";
